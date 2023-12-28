06c53f913fd830b3e3649e7321d060dc08db5d93
Tadeo Bennett <tadeos.bennett@gmail.com>
2023-12-28 11:26:45 -0600

    setup files(for learning, before project setup

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,2 +1,14 @@
+{{-- to say that it will be used in the layout --}}
+@extends('layout'); 
+
+
+{{-- wrapping this section and calling it 'content'.Its to be referenced in the layout.blade.php  --}}
+@section('content') 
+
 <h2>{{ $listing['title'] }}</h2>
 <p>{{ $listing['description'] }}</p>
+
+
+@endsection
+
+
